7db3c65562ea42d74891bdfaa95a99d4f0a59d62
Jevin Sweval <jevinsweval@gmail.com>
2024-08-14 11:10:50 -0400

    more options

--- cminify.cc
+++ cminify.cc
@@ -5,6 +5,11 @@
  * HandleTranslationUnit calls clang::tooling::applyAllReplacements.
  */
 
+// clang-format: off
+#undef NDEBUG
+#include <cassert>
+// clang-format: on
+
 #include <clang/AST/ASTConsumer.h>
 #include <clang/AST/Decl.h>
 #include <clang/AST/RecursiveASTVisitor.h>
@@ -26,14 +31,13 @@
 #include <llvm/ADT/DenseSet.h>
 #include <llvm/ADT/MapVector.h>
 #include <llvm/ADT/STLExtras.h>
-#include <llvm/TargetParser/Host.h>
 #include <llvm/Support/Path.h>
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/TargetParser/Host.h>
 
 #include <memory>
 #include <vector>
 
-#include <assert.h>
 #include <err.h>
 #include <unistd.h>
 
@@ -45,7 +49,8 @@ std::unique_ptr<CompilerInvocation> buildCompilerInvocation(ArrayRef<const char 
   IntrusiveRefCntPtr<DiagnosticsEngine> diags(
       CompilerInstance::createDiagnostics(new DiagnosticOptions, new IgnoringDiagConsumer, true));
 
-  clang::driver::Driver d{args[0], llvm::sys::getDefaultTargetTriple(), *diags, "cminify", llvm::vfs::getRealFileSystem()};
+  clang::driver::Driver d{args[0], llvm::sys::getDefaultTargetTriple(), *diags, "cminify",
+                          llvm::vfs::getRealFileSystem()};
   d.setCheckInputsExist(false);
   std::unique_ptr<clang::driver::Compilation> comp(d.BuildCompilation(args));
   if (!comp)
@@ -177,9 +182,10 @@ struct Renamer : RecursiveASTVisitor<Renamer> {
   }
 };
 
-struct MiniASTConsumer : ASTConsumer {
-  ASTContext *ctx;
-  int n_fn = 0, n_var = 0, n_type = 0;
+class MiniASTConsumer : public ASTConsumer {
+public:
+  MiniASTConsumer(bool remove_comments = false, bool rename_symbols = false)
+      : m_remove_comments{remove_comments}, m_rename_symbols{rename_symbols} {}
 
   void Initialize(ASTContext &ctx) override { this->ctx = &ctx; }
   static std::string getName(StringRef prefix, int &id) {
@@ -188,42 +194,49 @@ struct MiniASTConsumer : ASTConsumer {
     for (;;) {
       newName = std::string(1, prefix[id % prefix.size()]);
       if (int i = id / prefix.size())
-        while (newName += digits[i % 62], i /= 62);
+        while (newName += digits[i % 62], i /= 62)
+          ;
       id++;
-      if (!used.contains(CachedHashStringRef(newName))) break;
+      if (!used.contains(CachedHashStringRef(newName)))
+        break;
     }
     return newName;
   }
   bool HandleTopLevelDecl(DeclGroupRef dgr) override {
-    for (auto s : {"j0", "j1", "jn", "j0f", "j1f", "jnf", "j0l", "j1l", "jnl"})
-      used.insert(CachedHashStringRef(s));
-    for (auto s : {"y0", "y1", "yn", "y0f", "y1f", "ynf", "y0l", "y1l", "ynl"})
-      used.insert(CachedHashStringRef(s));
+    if (m_rename_symbols) {
+      for (auto s : {"j0", "j1", "jn", "j0f", "j1f", "jnf", "j0l", "j1l", "jnl"})
+        used.insert(CachedHashStringRef(s));
+      for (auto s : {"y0", "y1", "yn", "y0f", "y1f", "ynf", "y0l", "y1l", "ynl"})
+        used.insert(CachedHashStringRef(s));
 
-    Collector c(*ctx);
-    for (Decl *d : dgr)
-      c.TraverseDecl(d);
-    for (auto &[d, name] : d2name) {
-      if (name == "_f")
-        name = getName("abcdefghijklm", n_fn);
-      else if (name == "_v") {
-        int old_n_var = n_var;
-        auto newName = getName("nopqrstuvwxyz", n_var);
-        if (newName.size() < static_cast<VarDecl *>(d)->getName().size())
-          name = newName;
-        else {
-          name = static_cast<VarDecl *>(d)->getName();
-          n_var = old_n_var;
-        }
-      } else if (name == "_t")
-        name = getName("ABCDEFGHIJKLMNOPQRSTUVWXYZ", n_type);
+      Collector c(*ctx);
+      for (Decl *d : dgr)
+        c.TraverseDecl(d);
+      for (auto &[d, name] : d2name) {
+        if (name == "_f")
+          name = getName("abcdefghijklm", n_fn);
+        else if (name == "_v") {
+          int old_n_var = n_var;
+          auto newName = getName("nopqrstuvwxyz", n_var);
+          if (newName.size() < static_cast<VarDecl *>(d)->getName().size())
+            name = newName;
+          else {
+            name = static_cast<VarDecl *>(d)->getName();
+            n_var = old_n_var;
+          }
+        } else if (name == "_t")
+          name = getName("ABCDEFGHIJKLMNOPQRSTUVWXYZ", n_type);
+      }
     }
     return true;
   }
   void HandleTranslationUnit(ASTContext &ctx) override {
     tooling::Replacements reps;
-    Renamer c(ctx, reps);
-    c.TraverseDecl(ctx.getTranslationUnitDecl());
+
+    if (m_rename_symbols) {
+      Renamer c(ctx, reps);
+      c.TraverseDecl(ctx.getTranslationUnitDecl());
+    }
 
     auto &sm = ctx.getSourceManager();
     StringRef code = sm.getBufferData(sm.getMainFileID());
@@ -232,24 +245,93 @@ struct MiniASTConsumer : ASTConsumer {
       errx(2, "failed to apply replacements: %s", toString(res.takeError()).c_str());
     newCode = *res;
   }
+
+private:
+  ASTContext *ctx;
+  int n_fn = 0, n_var = 0, n_type = 0;
+  const bool m_remove_comments;
+  const bool m_rename_symbols;
 };
 
-struct MiniAction : ASTFrontendAction {
-  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &ci,
-                                                 StringRef inFile) override {
+class MiniAction : public ASTFrontendAction {
+public:
+  MiniAction(unsigned indent_width = 0, unsigned column_limit = 9999, bool remove_comments = false,
+             bool rename_symbols = false, bool space_before_assignment = false)
+      : m_indent_width{indent_width}, m_column_limit{column_limit}, m_remove_comments{remove_comments},
+        m_rename_symbols{rename_symbols}, m_space_before_assignment{space_before_assignment} {}
+  unsigned indent_width() const { return m_indent_width; }
+  unsigned column_limit() const { return m_column_limit; }
+  bool remove_comments() const { return m_remove_comments; }
+  bool rename_symbols() const { return m_rename_symbols; }
+  bool space_before_assignment() const { return m_space_before_assignment; }
+  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &ci, StringRef inFile) override {
     return std::make_unique<MiniASTConsumer>();
   }
+
+private:
+  const unsigned m_indent_width;
+  const unsigned m_column_limit;
+  const bool m_remove_comments;
+  const bool m_rename_symbols;
+  const bool m_space_before_assignment;
 };
 
-void reformat() {
+void reformat(const MiniAction &action) {
   auto buf = MemoryBuffer::getMemBuffer(newCode, "", true);
   format::FormatStyle style = cantFail(format::getStyle("LLVM", "-", "LLVM", newCode, nullptr));
-  style.ColumnLimit = 9999;
-  style.IndentWidth = 0;
-  style.ContinuationIndentWidth = 0;
-  style.SpaceBeforeAssignmentOperators = false;
+  style.ColumnLimit = action.column_limit();
+  style.IndentWidth = action.indent_width();
+  style.TabWidth = action.indent_width();
+  style.UseTab = format::FormatStyle::UT_Always;
+  style.ContinuationIndentWidth = action.indent_width();
+  style.SpaceBeforeAssignmentOperators = action.space_before_assignment();
   style.SpaceBeforeParens = format::FormatStyle::SBPO_Never;
   style.AlignEscapedNewlines = format::FormatStyle::ENAS_DontAlign;
+  style.AllowShortBlocksOnASingleLine = format::FormatStyle::SBS_Always;
+  style.AllowShortFunctionsOnASingleLine = format::FormatStyle::SFS_All;
+  style.AllowShortCaseLabelsOnASingleLine = true;
+  style.AllowShortEnumsOnASingleLine = true;
+  style.AllowShortIfStatementsOnASingleLine = format::FormatStyle::SIS_AllIfsAndElse;
+  style.AllowShortLambdasOnASingleLine = format::FormatStyle::SLS_All;
+  style.AllowShortLoopsOnASingleLine = true;
+  style.AlignConsecutiveAssignments = format::FormatStyle::AlignConsecutiveStyle{.Enabled = false};
+  style.AlignConsecutiveDeclarations = format::FormatStyle::AlignConsecutiveStyle{.Enabled = false};
+  style.AlignConsecutiveMacros = format::FormatStyle::AlignConsecutiveStyle{.Enabled = false};
+  style.AlignConsecutiveBitFields = format::FormatStyle::AlignConsecutiveStyle{.Enabled = false};
+  style.AlignArrayOfStructures = format::FormatStyle::AIAS_None;
+  style.AlignAfterOpenBracket = format::FormatStyle::BAS_DontAlign;
+  style.AlignConsecutiveShortCaseStatements = format::FormatStyle::ShortCaseStatementsAlignmentStyle{.Enabled = false};
+  style.AlignEscapedNewlines = format::FormatStyle::ENAS_DontAlign;
+  style.AlignOperands = format::FormatStyle::OAS_DontAlign;
+  style.AlignTrailingComments =
+      format::FormatStyle::TrailingCommentsAlignmentStyle{.Kind = format::FormatStyle::TCAS_Never};
+  style.AllowAllArgumentsOnNextLine = true;
+  // style.AllowShortCaseExpressionOnASingleLine = true; // clang-format 19
+  style.BinPackArguments = true;
+  style.BinPackParameters = true;
+  style.BitFieldColonSpacing = format::FormatStyle::BFCS_None;
+  style.BraceWrapping =
+      format::FormatStyle::BraceWrappingFlags{.AfterCaseLabel = false,
+                                              .AfterClass = false,
+                                              .AfterControlStatement = format::FormatStyle::BWACS_Never,
+                                              .AfterEnum = false,
+                                              .AfterFunction = false,
+                                              .AfterNamespace = false,
+                                              .AfterObjCDeclaration = false,
+                                              .AfterStruct = false,
+                                              .AfterUnion = false,
+                                              .AfterExternBlock = false,
+                                              .BeforeCatch = false,
+                                              .BeforeElse = false,
+                                              .BeforeLambdaBody = false,
+                                              .BeforeWhile = false,
+                                              .IndentBraces = false,
+                                              .SplitEmptyFunction = false,
+                                              .SplitEmptyRecord = false,
+                                              .SplitEmptyNamespace = false};
+  style.BreakStringLiterals = false;
+  style.BreakAdjacentStringLiterals = false;
+  // style.BreakBinaryOperations = format::FormatStyle::BBO_Never; // clang-format 20
 
   format::FormattingAttemptStatus status;
   std::vector<tooling::Range> ranges{{0, unsigned(newCode.size())}};
@@ -259,16 +341,27 @@ void reformat() {
     errx(2, "failed to apply replacements: %s", toString(res.takeError()).c_str());
   newCode = *res;
 }
-}
+} // namespace
 
 int main(int argc, char *argv[]) {
   std::vector<const char *> args{argv[0], "-fsyntax-only"};
   bool inplace = false;
+  bool rename_syms = false;
+  bool remove_comments = false;
+  bool space_before_assignment = false;
+  unsigned indent = 0;
+  unsigned columns = 9999;
   const char *outfile = "/dev/stdout";
-  const char usage[] = R"(Usage: %s [-i] [-f fun]... a.c
+  const char usage[] = R"(Usage: %s [-i] [-r] [-c] [-a] [-I <indent>] [-C <column>] [-f fun]... a.c
 
 Options:
 -i      edit a.c in place
+-r      rename symbols (default: false)
+-c      remove comments (default: false)
+-a      space before assignment operators (default: false)
+-I      indent width (default: 0)
+-C      column limit (default: 9999)
+-f      ignore function (can be repeated)
 )";
   for (int i = 1; i < argc; i++) {
     StringRef opt(argv[i]);
@@ -279,12 +372,26 @@ Options:
       return 0;
     } else if (opt == "-i")
       inplace = true;
-    else if (opt == "-f" && i + 1 < argc)
+    else if (opt == "-r")
+      rename_syms = true;
+    else if (opt == "-c")
+      remove_comments = true;
+    else if (opt == "-a")
+      space_before_assignment = true;
+    else if (opt == "-I" && i + 1 < argc) {
+      auto sindent = atoi(argv[++i]);
+      assert(sindent >= 0);
+      indent = static_cast<unsigned>(sindent);
+    } else if (opt == "-C" && i + 1 < argc) {
+      auto scol = atoi(argv[++i]);
+      assert(scol >= 0);
+      columns = static_cast<unsigned>(scol);
+    } else if (opt == "-f" && i + 1 < argc)
       ignores.push_back(argv[++i]);
     else if (opt == "-o" && i + 1 < argc)
       outfile = argv[++i];
     else {
-      fputs(usage, stderr);
+      fprintf(stderr, usage, getprogname());
       return 1;
     }
   }
@@ -305,13 +412,13 @@ Options:
   inst->createFileManager(llvm::vfs::getRealFileSystem());
   inst->setSourceManager(new SourceManager(inst->getDiagnostics(), inst->getFileManager(), true));
 
-  MiniAction action;
+  MiniAction action{indent, columns, remove_comments, rename_syms, space_before_assignment};
   if (!action.BeginSourceFile(*inst, inst->getFrontendOpts().Inputs[0]))
     errx(2, "failed to parse");
   if (Error e = action.Execute())
     errx(2, "failed to execute");
   action.EndSourceFile();
-  reformat();
+  reformat(action);
 
   std::error_code ec;
   raw_fd_ostream(inplace ? inst->getFrontendOpts().Inputs[0].getFile() : outfile, ec, sys::fs::OF_None) << newCode;
